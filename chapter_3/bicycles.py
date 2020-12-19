6806016b473b6e0982797556991b65389ad5e43b
Philippe Conway <philippe.conway@gmail.com>
2020-12-18 16:49:57 -0800

    Added more examples

--- chapter_3/bicycles.py
+++ chapter_3/bicycles.py
@@ -1,2 +1,15 @@
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
+
+# Using the list
+print(bicycles[0])
+print(bicycles[0].title())
+print(bicycles[1])
+print(bicycles[3])
+
+# Access the last entry in the list
+print(bicycles[-1])
+
+# Using individual values from a list
+message = f"My first bicycle was a {bicycles[0].title()}"
+print(message)
