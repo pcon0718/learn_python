6a59573391eac6b55d5b609848f9e4992422125f
Philippe Conway <philippe.conway@gmail.com>
2020-12-19 22:06:02 -0800

    initial commit

---
+++ chapter_3/exercise3-4.py
@@ -0,0 +1,9 @@
+guest_list = ['bono', 'edge', 'adam', 'larry']
+guest1 = guest_list.pop(0)
+guest2 = guest_list.pop(0)
+guest3 = guest_list.pop(0)
+guest4 = guest_list.pop(0)
+print(f"Hello {guest1.title()}. Welcome.")
+print(f"Hello {guest2.title()}. Welcome.")
+print(f"Hello {guest3.title()}. Welcome.")
+print(f"Hello {guest4.title()}. Welcome.")
