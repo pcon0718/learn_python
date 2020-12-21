74f3b68d90d13ccbab910a87205e8105b50a26fc
Philippe Conway <philippe.conway@gmail.com>
2020-12-21 00:14:08 -0800

    Inital commit

---
+++ chapter_4/exercise4-4.py
@@ -0,0 +1,6 @@
+# Create a list with numbers 1-1000
+numbers = list(range(1, 1_001))
+
+# Print out each number.
+for number in numbers:
+    print(number)
---
+++ chapter_4/exercise4-5.py
@@ -0,0 +1,6 @@
+# Create a list with numbers 1-1000
+numbers = list(range(1, 1_001))
+
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
