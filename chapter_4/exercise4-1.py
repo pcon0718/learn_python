2d6aed3f2416ccebbd8247c5fd5fdd19fcae5ef8
Philippe Conway <philippe.conway@gmail.com>
2020-12-20 22:54:01 -0800

    initial commit

---
+++ chapter_4/exercise4-1.py
@@ -0,0 +1,5 @@
+pizzas = ['cheese', 'supreme', 'pepperoni']
+for pizza in pizzas:
+    print(f"I like {pizza} pizza.")
+
+print("Pizza is one of my favorite foods!")
---
+++ chapter_4/exercise4-2.py
@@ -0,0 +1,6 @@
+animals = ['dog', 'cat', 'koala']
+for animal in animals:
+    print(f"A {animal} would make a great pet.")
+
+count = len(animals)
+print(f"All {count} of these animals are fluffy.")
