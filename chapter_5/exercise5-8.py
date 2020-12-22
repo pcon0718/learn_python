c59cb2b4103e239cf44bbba20782b1e416c3d888
Philippe Conway <philippe.conway@gmail.com>
2020-12-22 15:15:55 -0800

    initial commit

---
+++ chapter_5/exercise5-8.py
@@ -0,0 +1,7 @@
+users = ["admin", "pconway", "lmccoy", "nuhura", "jkirk"]
+for user in users:
+    if user == 'admin':
+        print(f"Hello {user}, would you like to see a status report?")
+    else:
+        print(f"Hello {user}, thank you for logging in again.")
+
