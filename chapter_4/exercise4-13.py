63b6c1ce83aff0b82df7fd1d7f597842fbc24ff2
Philippe Conway <philippe.conway@gmail.com>
2020-12-21 16:56:35 -0800

    initial commit

---
+++ chapter_4/exercise4-13.py
@@ -0,0 +1,9 @@
+foods = ('rice', 'chicken', 'steak', 'fish', 'potatoes')
+print("The following foods are in the buffet:")
+for food in foods:
+    print(food)
+
+foods = ('rice', 'chicken', 'steak', 'lobster', 'french fries')
+print("\n These are the new foods in the buffet:")
+for food in foods:
+    print(food)
