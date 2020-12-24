0c8d4ed058472cf06f6e4bf2f66a664232f87f9f
Philippe Conway <philippe.conway@gmail.com>
2020-12-23 16:07:17 -0800

    initial commit

---
+++ chapter_6/exercise6-1.py
@@ -0,0 +1,10 @@
+person_1 = {
+    "first_name": 'james',
+    'last_name': 'kirk',
+    'age': '32',
+    'city': 'riverside',
+}
+print(person_1['first_name'].title())
+print(person_1['last_name'].title())
+print(person_1['age'])
+print(person_1['city'].title())
---
+++ chapter_6/exercise6-2.py
@@ -0,0 +1,12 @@
+favorite_numbers = {
+    'james': 1,
+    'jean luc': 2,
+    'ben': 3,
+    'katheryn': 4,
+    'jonathan': 5,
+}
+print(favorite_numbers['james'])
+print(favorite_numbers['jean luc'])
+print(favorite_numbers['ben'])
+print(favorite_numbers['katheryn'])
+print(favorite_numbers['jonathan'])
