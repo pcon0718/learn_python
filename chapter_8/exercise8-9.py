bf2eb98dc6a72a83b159ed68a08fc817a45f2073
Philippe Conway <philippe.conway@gmail.com>
2021-01-24 22:13:25 -0800

    initial commit

---
+++ chapter_8/exercise8-9.py
@@ -0,0 +1,6 @@
+def show_messages(text_messages):
+    for text_message in text_messages:
+        print(text_message)
+
+text_messages = ['Hello there!', 'Pick me up some apples', 'Lets meet for dinner tonight.']
+show_messages(text_messages)
